cfe6e6258eb0bf7f23187c52a8afcbe4ec7be3fe
Samantha Callie <Hoomee90@gmail.com>
2026-01-07 13:47:18 -0800

    create project

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HttpCat from './HttpCat';
+
+function App() {
+  return (
+    <React.Fragment>
+      < HttpCat />
+    </React.Fragment>
+  );
+}
+
+export default App;
