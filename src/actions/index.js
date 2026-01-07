f9e6e086776cc70476c9c6eca9b7a5becfa03ac4
Samantha Callie <Hoomee90@gmail.com>
2026-01-07 13:48:30 -0800

    create actions

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import * as c from './ActionTypes';
+
+export const getCatImageSuccess = (catImage) => ({
+  type: c.GET_CAT_IMAGE_SUCCESS,
+  catImage
+});
+
+export const getCatImageFailure = (error) => ({
+  type: c.GET_CAT_IMAGE_FAILURE,
+  error
+});
